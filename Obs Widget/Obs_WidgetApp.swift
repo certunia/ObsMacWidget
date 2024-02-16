42c9ecaeb12da1e72c7385979d9107a78cb2bf73
dtuneev <dtuneev@mvpbg.com>
2024-02-16 12:38:03 +0200

    Initial Commit

---
+++ Obs Widget/Obs_WidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Obs_WidgetApp.swift
+//  Obs Widget
+//
+//  Created by Daniel Tiuneev on 16.02.24.
+//
+
+import SwiftUI
+
+@main
+struct Obs_WidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
